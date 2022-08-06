77ec65b123a010d35103128e58d7c90b43feb4bb
itzjustalan <itzjustalan@gmail.com>
2022-08-06 18:59:49 +0530

    constructor function

---
+++ constants.go
@@ -0,0 +1,23 @@
+package whatsapp_cloud_api_go
+
+const (
+	HTTP  HttpProtocol = "http"
+	HTTPS              = "https"
+)
+
+const (
+	V1_0  ApiVersion = "v1.0"
+	V2_0             = "v2.0"
+	V3_0             = "v3.0"
+	V4_0             = "v4.0"
+	V5_0             = "v5.0"
+	V6_0             = "v6.0"
+	V7_0             = "v7.0"
+	V8_0             = "v8.0"
+	V9_0             = "v9.0"
+	V10_0            = "v10.0"
+	V11_0            = "v11.0"
+	V12_0            = "v12.0"
+	V13_0            = "v13.0"
+	V14_0            = "v14.0"
+)
--- whatsapp-cloud-api-go.go
+++ whatsapp-cloud-api-go.go
@@ -1,5 +1,32 @@
 package whatsapp_cloud_api_go
 
+type WhatsApp interface{}
+
+type whatsApp struct {
+	AccessToken    string
+	ApiVersion     ApiVersion
+	BaseUrl        string
+	FromPhone      string
+	HostDomainName string
+	HttpProtocol   HttpProtocol
+}
+
+type HttpProtocol string
+type ApiVersion string
+
+func New(phone, token string, version ApiVersion, protocol HttpProtocol) WhatsApp {
+	domain := "graph.facebook.com"
+	url := string(protocol) + "://" + domain + "/" + string(version)
+	return whatsApp{
+		AccessToken:    token,
+		ApiVersion:     version,
+		BaseUrl:        url,
+		FromPhone:      phone,
+		HttpProtocol:   protocol,
+		HostDomainName: domain,
+	}
+}
+
 // add a and b
 func Add(a int, b int) int {
 	// return the sum
